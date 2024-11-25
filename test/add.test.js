46adad5a173195e3282cbd96fcca77a1a6454dd8
tfheju <henri.jussila@tuni.fi>
2024-11-25 10:21:39 +0200

    Debugging coverages

--- test/add.test.js
+++ test/add.test.js
@@ -9,26 +9,4 @@ describe('Array', function () {
   });
 });
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return 12 when adding 5 and 7', function () {
-      assert.equal(add(5,7), 12);
-    });
-  });
-});
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return #1 when adding # and 1', function () {
-      assert.equal(add('#', 1), '#1');
-    });
-  });
-});
-
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return 0 when adding -1 and 1', function () {
-      assert.equal(add(-1, 1), 0);
-    });
-  });
-});
