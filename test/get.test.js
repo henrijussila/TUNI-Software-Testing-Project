388118255bb05d226e2d228ae7e5229f26b09ed4
tfheju <henri.jussila@tuni.fi>
2024-12-05 20:31:42 +0200

    Added additional tests for get.test.js

--- test/get.test.js
+++ test/get.test.js
@@ -14,7 +14,7 @@ describe('get', function () {
         assert.strictEqual(get(object, ['a', '0', 'b', 'c']), 3);
     });
 
-    //NEGATIVE TESTS
+    // NEGATIVE TESTS
     it('should return the default value if the resolved value is undefined', function () {
         const object = { 'a': [{ 'b': { 'c': 3 } }] };
         assert.strictEqual(get(object, 'a.b.c', 'default'), 'default');
@@ -25,9 +25,24 @@ describe('get', function () {
         assert.strictEqual(get(object, 'a.x.y'), undefined);
     });
 
+    it('should return undefined for invalid path formats', function () {
+        const object = { a: { b: { c: 3 } } };
+        assert.strictEqual(get(object, 'a..b.c'), undefined);
+    });
+
+    it('should return undefined for an empty path string', function () {
+        const object = { a: 10 };
+        assert.strictEqual(get(object, ''), undefined);
+        assert.strictEqual(get(object, '', 'default'), 'default');
+    });
+
     // LIMIT VALUE TESTS
     it('should handle null or undefined objects gracefully', function () {
         assert.strictEqual(get(null, 'a.b.c', 'default'), 'default');
         assert.strictEqual(get(undefined, 'a.b.c', 'default'), 'default');
+    });
+
+    it('should return the default value for an empty object', function () {
+        assert.strictEqual(get({}, 'a.b.c', 'default'), 'default');
     });
 });
